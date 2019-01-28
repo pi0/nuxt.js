7f80ce37a2c6d33847d17a6090b403074aa21e5f
pooya parsa <pooya@pi0.ir>
2019-01-28 15:49:13 +0330

    fix(cluster): proxy args by default

--- packages/worker/src/cluster.js
+++ packages/worker/src/cluster.js
@@ -7,7 +7,12 @@ cluster.on('exit', (worker, code, signal) => {
 })
 
 // eslint-disable-next-line require-await
-export async function forkWorker(args = process.argv.slice(2)) {
+export async function forkWorker(...args) {
+  // Proxy args by default
+  if (!args.length) {
+    args = process.argv.slice(2)
+  }
+
   // Setup master setting for entrypoint
   cluster.setupMaster({
     exec: require.resolve('@nuxt/worker/bin/nuxt-worker'),
